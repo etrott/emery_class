d7e958e237aa1668189dddffac41a1d82f18be3a
etrott <etrott@haverford.edu>
2017-07-17 16:17:48 -0400

    Update fisher_deriv.py

--- fisher_deriv.py
+++ fisher_deriv.py
@@ -2,6 +2,19 @@ from classy import Class
 import numpy as np
 import matplotlib.pyplot as plt
 import subprocess
+import matplotlib as mpl
+
+font = {'family':'serif','weight':'normal','size':12}
+mpl.rcParams['xtick.major.size'] = 7
+mpl.rcParams['xtick.major.width'] = 1
+mpl.rcParams['xtick.minor.size'] = 3
+mpl.rcParams['xtick.minor.width'] = 1
+mpl.rcParams['ytick.major.size'] = 7
+mpl.rcParams['ytick.major.width'] = 1
+mpl.rcParams['ytick.minor.size'] = 3
+mpl.rcParams['ytick.minor.width'] = 1
+mpl.rcParams['axes.labelsize'] = 16
+mpl.rc('font', **font)
 
 def run_fisher(param,mass,cv,delta,num,deg,run = 'False'):
     def run_CLASS(dict):
@@ -33,7 +46,8 @@ def run_fisher(param,mass,cv,delta,num,deg,run = 'False'):
     runs = np.reshape([cv_floats]*len(cv_floats)*num,(len(cv_floats),num,len(cv_floats)))
     for i in range(len(runs)):
         runs[i][:,i] = span[i]
-
+    print runs
+    
     def save():
         ell = []
         cl_tt = []
@@ -68,20 +82,30 @@ def run_fisher(param,mass,cv,delta,num,deg,run = 'False'):
         return ell,cl_tt,cl_te,cl_ee
 
     ell,cl_tt,cl_te,cl_ee = load()
-
+    
     ell = np.transpose(ell,(0,2,1))
     cl_tt = np.transpose(cl_tt,(0,2,1))
+    cl_te = np.transpose(cl_te,(0,2,1))
+    cl_ee = np.transpose(cl_ee,(0,2,1))
+
+    def poly_fit(x,y,deg):
+        poly = []
+        for i in range(len(y)):
+            for j in range(len(y[i])):
+                poly.append(np.polyfit(x[i],y[i][j],deg))
+        poly = np.reshape(poly,(len(y),len(y[0]),deg+1))
+        return poly
 
     def deriv(cl):
         poly_coeff = poly_fit(span,cl,deg)
-        #print poly_coeff
+#        print poly_coeff                                                                                                                                                                         
         deriv_coeff = []
         for i in range(len(poly_coeff)):
             for j in range(len(poly_coeff[i])):
                 deriv_coeff.append(np.poly1d(np.polyder(poly_coeff[i][j])))
-        #deriv_coeff = np.reshape(deriv_coeff,(len(poly_coeff),len(poly_coeff[0]),len(poly_coeff[0][0])-1))
+#    deriv_coeff = np.reshape(deriv_coeff,(len(poly_coeff),len(poly_coeff[0]),len(poly_coeff[0][0])-1))                                                                                           
         deriv_coeff = np.reshape(deriv_coeff,(len(poly_coeff),len(poly_coeff[0]),1))
-        #print deriv_coeff
+#        print deriv_coeff                                                                                                                                                                        
         deriv = []
         for i in range(len(deriv_coeff)):
             for j in range(len(deriv_coeff[i])):
@@ -97,7 +121,7 @@ def run_fisher(param,mass,cv,delta,num,deg,run = 'False'):
         fig,ax = plt.subplots()
         for i in range(len(deriv)):
             ax.plot(x[0],y[i],label = r'$%s$' %(param_labels[i]))
-#        plt.yscale('log')
+#        plt.yscale('log')                                                                                                                                                                        
         plt.xlabel(r'$l$')
         plt.ylabel(r'$\partial{C_l^{TT}}/\partial{p}$')
         plt.title(r'CMB Power Spectrum Derivatives')
@@ -105,19 +129,31 @@ def run_fisher(param,mass,cv,delta,num,deg,run = 'False'):
         plt.savefig('deriv.pdf')
         subprocess.call('open deriv.pdf',shell = True)
 
-    plot(ell[:,:,12],deriv)
-
-#next step -- figure out a generalized way (i.e. for any degree fit) to convert the coefficients into derivatives
-
-"""
-    def fisher(deriv):
+#    plot(ell[:,:,12],deriv)        
+    def fisher(deriv,ell,cl,npix,sigma,theta):
+        sigma = theta/np.sqrt(8*np.log(2))
+        ell = ell[:,:,12]
+        cl = cl[:,:,12]
+        npix = np.asarray(npix).reshape(1,1)
+        sigma = np.asarray(sigma).reshape(1,1)
+        theta = np.asarray(theta).reshape(1,1)
         matrix = []
+#        n = 4*np.pi*np.square(sigma)/npix                                                                                                                                                        
+#        w = np.exp(-np.square(ell)*np.square(theta)/(16*np.log(2)))                                                                                                                              
+#        err = 2*(cl+n*np.power(w,-2))/(2*ell+1)                                                                                                                                                  
+#        err = 2*(cl+n)/(2*ell+1)                                                                                                                                                                 
+        for i in range(len(deriv)):
+            for j in range(len(deriv[i])):
+                if np.isinf(err[i][j]) == True:
+                    err[i][j] = 0
         for i in range(len(deriv)):
             for j in range(len(deriv)):
+#                matrix.append(np.sum(deriv[i]*deriv[j]*err[i]))                                                                                                                                  
                 matrix.append(np.sum(deriv[i]*deriv[j]))
-        matrix = np.reshape(matrix,(len(num_cv),len(num_cv)))
+        matrix = np.reshape(matrix,(len(deriv),len(deriv)))
         return matrix
-    return fisher(deriv)
-"""
+
+#    return fisher(deriv,ell,cl_tt,61,1,0.02217)                                                                                                                                                  
 
 fisher_matrix = run_fisher(['output','lensing','Omega_b','Omega_cdm','H0'],[1.0],['tCl,pCl,lCl','yes',0.02234,0.1189,67.8],[0.00023,0.0022,1.0],25,3,run = 'False')
+print fisher_matrix
