f1903201685d1a1192342e3c5492598f5b1261a1
etrott <etrott@haverford.edu>
2017-07-17 23:26:02 -0400

    fixed merge issues

--- fisher_deriv.py
+++ fisher_deriv.py
@@ -16,7 +16,7 @@ mpl.rcParams['ytick.minor.width'] = 1
 mpl.rcParams['axes.labelsize'] = 16
 mpl.rc('font', **font)
 
-def run_fisher(param,mass,cv,delta,num,deg,run = 'False'):
+def run_fisher(param,cv,delta,mass = [1.0],num = 25,deg = 3,run = 'False'):
     def run_CLASS(dict):
         cosmo = Class()
         cosmo.set(dict)
@@ -65,19 +65,19 @@ def run_fisher(param,mass,cv,delta,num,deg,run = 'False'):
         cl_tt = np.reshape(cl_tt,(len(cv_floats),num,2501))
         cl_te = np.reshape(cl_te,(len(cv_floats),num,2501))
         cl_ee = np.reshape(cl_ee,(len(cv_floats),num,2501))
-        np.save('mat_ell',ell)
-        np.save('mat_cl_tt',cl_tt)
-        np.save('mat_cl_te',cl_te)
-        np.save('mat_cl_ee',cl_ee)
+        np.save('ell',ell)
+        np.save('cl_tt',cl_tt)
+        np.save('cl_te',cl_te)
+        np.save('cl_ee',cl_ee)
 
     if run == 'True':
         save()
 
     def load():
-        ell = np.load('mat_ell.npy')
-        cl_tt = np.load('mat_cl_tt.npy')
-        cl_te = np.load('mat_cl_te.npy')
-        cl_ee = np.load('mat_cl_ee.npy')
+        ell = np.load('ell.npy')
+        cl_tt = np.load('cl_tt.npy')
+        cl_te = np.load('cl_te.npy')
+        cl_ee = np.load('cl_ee.npy')
         return ell,cl_tt,cl_te,cl_ee
 
     ell,cl_tt,cl_te,cl_ee = load()
@@ -117,7 +117,6 @@ def run_fisher(param,mass,cv,delta,num,deg,run = 'False'):
         fig,ax = plt.subplots()
         for i in range(len(deriv)):
             ax.plot(x[0],y[i],label = r'$%s$' %(param_labels[i]))
-#        plt.yscale('log')
         plt.xlabel(r'$l$')
         plt.ylabel(r'$\partial{C_l^{TT}}/\partial{p}$')
         plt.title(r'CMB Power Spectrum Derivatives')
@@ -126,24 +125,6 @@ def run_fisher(param,mass,cv,delta,num,deg,run = 'False'):
         subprocess.call('open deriv.pdf',shell = True)
 
 #    plot(ell[:,:,12],deriv)
-
-    deriv = deriv(cl_tt)
-
-    def plot(x,y):
-        y_mod = x*(x+1.0)*y/(2*np.pi)
-        param_labels = ['\omega_b','\omega_{cdm}','H_0']
-        fig,ax = plt.subplots()
-        for i in range(len(deriv)):
-            ax.plot(x[0],y[i],label = r'$%s$' %(param_labels[i]))
-#        plt.yscale('log')                                                                                                                                                                        
-        plt.xlabel(r'$l$')
-        plt.ylabel(r'$\partial{C_l^{TT}}/\partial{p}$')
-        plt.title(r'CMB Power Spectrum Derivatives')
-        plt.legend(frameon = False, loc = 'upper right')
-        plt.savefig('deriv.pdf')
-        subprocess.call('open deriv.pdf',shell = True)
-
-#    plot(ell[:,:,12],deriv)        
 
     def fisher(deriv,ell,cl,npix,sigma,theta):
         sigma = theta/np.sqrt(8*np.log(2))
@@ -156,11 +137,10 @@ def run_fisher(param,mass,cv,delta,num,deg,run = 'False'):
 #        n = 4*np.pi*np.square(sigma)/npix
 #        w = np.exp(-np.square(ell)*np.square(theta)/(16*np.log(2)))
 #        err = 2*(cl+n*np.power(w,-2))/(2*ell+1)
-#        err = 2*(cl+n)/(2*ell+1)
-        for i in range(len(deriv)):
-            for j in range(len(deriv[i])):
-                if np.isinf(err[i][j]) == True:
-                    err[i][j] = 0
+#        for i in range(len(deriv)):
+#            for j in range(len(deriv[i])):
+#                if np.isinf(err[i][j]) == True:
+#                    err[i][j] = 0
         for i in range(len(deriv)):
             for j in range(len(deriv)):
 #                matrix.append(np.sum(deriv[i]*deriv[j]*err[i]))
@@ -168,9 +148,9 @@ def run_fisher(param,mass,cv,delta,num,deg,run = 'False'):
         matrix = np.reshape(matrix,(len(deriv),len(deriv)))
         return matrix
 
-#    return fisher(deriv,ell,cl_tt,61,1,0.02217)
+    return fisher(deriv,ell,cl_tt,61,1,0.02217)
 
 
-#fisher_matrix = run_fisher(['output','lensing','Omega_b','Omega_cdm','H0'],[1.0],['tCl,pCl,lCl','yes',0.02234,0.1189,67.8],[0.00023,0.0022,1.0],25,3,run = 'False')
-fisher_matrix = run_fisher(['output','lensing','Omega_b','Omega_cdm','H0'],[1.0],['tCl,pCl,lCl','yes',0.0222,0.1197,67.31],[0.00022,0.0012,0.67],5,3,run = 'True')
+fisher_matrix = run_fisher(['output','lensing','Omega_b','Omega_cdm','H0'],['tCl,pCl,lCl','yes',0.02234,0.1189,67.8],[0.00023,0.0022,1.0],run = 'False')
+#fisher_matrix = run_fisher(['output','lensing','Omega_b','Omega_cdm','H0'],[1.0],['tCl,pCl,lCl','yes',0.0222,0.1197,67.31],[0.00022,0.0012,0.67],5,3,run = 'True')
 print fisher_matrix
